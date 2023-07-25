e17b8af60584c25501ef6da773f1bc30e694c4d5
SoftwareHeart <ertugrulkundak@hotmail.com>
2023-07-25 12:48:32 +0300

    Responsive çalışmaları Yapıldı

--- js/script.js
+++ js/script.js
@@ -113,3 +113,89 @@ menuButton.addEventListener('click', function() {
 closeButton.addEventListener('click', function() {
   menu.classList.remove('menu-open');
 });
+
+
+//Acces the Images
+let slideImages = document.querySelectorAll('.buyuk-slides img');
+//Acces the buttons
+let next = document.querySelector('.buttons-buyuk-slider .next');
+let prev = document.querySelector('.buttons-buyuk-slider .prev');
+//Acces the dots
+let dots = document.querySelectorAll('.dot');
+
+var counter = 0;
+
+//Next Button
+next.addEventListener('click', slideNext);
+function slideNext() {
+    slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+    if(counter >= slideImages.length - 1) {
+        counter = 0;
+    }else {
+        counter++;
+    }
+    slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+    indicators();
+}
+
+//Prev Button
+prev.addEventListener('click', slidePrev);
+function slidePrev() {
+    slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+    if(counter == 0) {
+        counter = slideImages.length - 1;
+    }else {
+        counter--;
+    }
+    slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+    indicators();
+}
+
+//Auto Slide
+function autoSlide() {
+    deletInterval = setInterval(timer, 3000);
+    function timer() {
+        slideNext();
+        indicators();
+    }
+}
+autoSlide();
+
+//Stop Auto Slide
+const container = document.querySelector('.buyuk-slide-container');
+container.addEventListener('mouseover', function() {
+    clearInterval(deletInterval);
+});
+
+//Rasume Auto Sliding when mouseout
+container.addEventListener('mouseout',autoSlide); 
+
+//Add active class to dots
+function indicators(){
+    for(i=0; i< dots.length; i++){
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    dots[counter].className += ' active';
+}
+
+//Add click event to dots
+function switchImage(currentImage){
+    currentImage.classList.add('active');
+    var imageId = currentImage.getAttribute('attr');
+    if(imageId>counter){
+        slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+        counter = imageId;
+        slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+    }
+    else if(imageId==counter){
+        return;
+    }
+    else{
+        slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+        counter = imageId;
+        slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+    }
+    indicators();
+}
+
+

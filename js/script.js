7fef12579a8bc987b26d48e4f8489a34f1976362
SoftwareHeart <ertugrulkundak@hotmail.com>
2023-07-26 14:19:43 +0300

    @SoftwareHeart Daha Fazlası kısmı eklendi

--- js/script.js
+++ js/script.js
@@ -4,19 +4,26 @@ const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".slide-wrapper i");
 const carouselChildrens = [...carousel.children];
 
-let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
 
 // Get the number of cards that can fit in the carousel at once
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
 // Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+carouselChildrens
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
+  });
 
 // Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
 // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
@@ -25,55 +32,58 @@ carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("no-transition");
 
 // Add event listeners for the arrow buttons to scroll the carousel left and right
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-    });
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
 });
 
 const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
-}
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
 
 const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
 
 const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
 
 const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
+  // If the carousel is at the beginning, scroll to the end
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+  // If the carousel is at the end, scroll to the beginning
+  else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
 
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
-    clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay();
-}
+  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
 
 const autoPlay = () => {
-    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-}
+  if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+  // Autoplay the carousel after every 2500 ms
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2500);
+};
 autoPlay();
 
 carousel.addEventListener("mousedown", dragStart);
@@ -83,127 +93,119 @@ carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
 
-
 //sticky
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('.header');
-    
-    if (window.scrollY > 0) {
-      header.classList.add('header-sticky');
-    } else {
-      header.classList.remove('header-sticky');
-    }
-  });
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".header");
 
-  
+  if (window.scrollY > 0) {
+    header.classList.add("header-sticky");
+  } else {
+    header.classList.remove("header-sticky");
+  }
+});
 
-carousel.addEventListener("mousedown",dragStart);
-carousel.addEventListener("mousemove",dragging);
-document.addEventListener("mouseup",dragStop);
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 
-var menuButton = document.querySelector('.menu-button');
-var menu = document.querySelector('.menu');
-var closeButton = document.querySelector('.close-button');
+var menuButton = document.querySelector(".menu-button");
+var menu = document.querySelector(".menu");
+var closeButton = document.querySelector(".close");
 
-menuButton.addEventListener('click', function() {
-  menu.classList.toggle('menu-open');
+menuButton.addEventListener("click", function () {
+  menu.classList.toggle("menu-open");
 });
 
-closeButton.addEventListener('click', function() {
-  menu.classList.remove('menu-open');
+closeButton.addEventListener("click", function () {
+  menu.classList.remove("menu-open");
 });
-
 
 //Acces the Images
-let slideImages = document.querySelectorAll('.buyuk-slides .buyuk-slides-item #resim-1');
+let slideImages = document.querySelectorAll(
+  ".buyuk-slides .buyuk-slides-item #resim-1"
+);
 //Acces the buttons
-let next = document.querySelector('.buttons-buyuk-slider .next');
-let prev = document.querySelector('.buttons-buyuk-slider .prev');
+let next = document.querySelector(".buttons-buyuk-slider .next");
+let prev = document.querySelector(".buttons-buyuk-slider .prev");
 //Acces the dots
-let dots = document.querySelectorAll('.dot');
-let paragraf = document.querySelectorAll('.buyuk-slides .buyuk-slides-item .buyuk-slide-icerik .buyuk-slide-paragraf');
+let dots = document.querySelectorAll(".dot");
+let paragraf = document.querySelectorAll(
+  ".buyuk-slides .buyuk-slides-item .buyuk-slide-icerik .buyuk-slide-paragraf"
+);
 //span
-
 
 var counter = 0;
 
 //Next Button
-next.addEventListener('click', slideNext);
+next.addEventListener("click", slideNext);
 function slideNext() {
-   
-    slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
-    if(counter >= slideImages.length - 1) {
-        counter = 0;
-    }else {
-        counter++;
-    }
-    slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-    indicators();
-    
+  slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
+  if (counter >= slideImages.length - 1) {
+    counter = 0;
+  } else {
+    counter++;
+  }
+  slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
+  indicators();
 }
 
 //Prev Button
-prev.addEventListener('click', slidePrev);
+prev.addEventListener("click", slidePrev);
 function slidePrev() {
-    slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
-    if(counter == 0) {
-        counter = slideImages.length - 1;
-    }else {
-        counter--;
-    }
-    slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
-    indicators();
-
+  slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
+  if (counter == 0) {
+    counter = slideImages.length - 1;
+  } else {
+    counter--;
+  }
+  slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
+  indicators();
 }
 
 //Auto Slide
-// function autoSlide() {
-//     deletInterval = setInterval(timer, 3000);
-//     function timer() {
-//         slideNext();
-//         indicators();
-//     }
-// }
-// autoSlide();
+function autoSlide() {
+  deletInterval = setInterval(timer, 5000);
+  function timer() {
+    slideNext();
+    indicators();
+  }
+}
+autoSlide();
 
 //Stop Auto Slide
-const container = document.querySelector('.buyuk-slide-container');
-container.addEventListener('mouseover', function() {
-    clearInterval(deletInterval);
+const container = document.querySelector(".buyuk-slide-container");
+container.addEventListener("mouseover", function () {
+  clearInterval(deletInterval);
 });
 
 //Rasume Auto Sliding when mouseout
-container.addEventListener('mouseout',autoSlide); 
+container.addEventListener("mouseout", autoSlide);
 
 //Add active class to dots
-function indicators(){
-    for(i=0; i< dots.length; i++){
-        dots[i].className = dots[i].className.replace(' active', '');
-        paragraf[i].className = paragraf[i].className.replace(' active', '');
-    }
-    dots[counter].className += ' active';
-    paragraf[counter].className += ' active';
+function indicators() {
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+    paragraf[i].className = paragraf[i].className.replace(" active", "");
+  }
+  dots[counter].className += " active";
+  paragraf[counter].className += " active";
 }
 
 //Add click event to dots
-function switchImage(currentImage){
-    currentImage.classList.add('active');
-    var imageId = currentImage.getAttribute('attr');
-    if(imageId>counter){
-        slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
-        counter = imageId;
-        slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-    }
-    else if(imageId==counter){
-        return;
-    }
-    else{
-        slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
-        counter = imageId;
-        slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
-    }
-    indicators();
+function switchImage(currentImage) {
+  currentImage.classList.add("active");
+  var imageId = currentImage.getAttribute("attr");
+  if (imageId > counter) {
+    slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
+    counter = imageId;
+    slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
+  } else if (imageId == counter) {
+    return;
+  } else {
+    slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
+    counter = imageId;
+    slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
+  }
+  indicators();
 }
-
-
